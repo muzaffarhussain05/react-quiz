0e276d14a31bc37e1912b5fcd7f888e65e095de9
rubina557 <rubinanazeer29@gmail.com>
2025-08-05 19:41:26 +0500

    feat(ui): navbar, start screen and layout

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          "Oxygen",
+          "Ubuntu",
+          "Cantarell",
+          '"Open Sans"',
+          '"Helvetica Neue"',
+          "sans-serif",
+        ],
+        codystar: ["Codystar", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
